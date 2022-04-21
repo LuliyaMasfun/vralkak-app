f492aca1f20c071a03b3775f2e0e67fd10862cd6
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-04-21 20:05:27 +0200

    Validation done

--- LoginPage/SignUpPage.js
+++ LoginPage/SignUpPage.js
@@ -1,18 +1,23 @@
 
-
+var namn = sessionStorage.getItem("UserName");
+document.getElementById("greetingUser").innerHTML ="Välkommen in, " + namn +"!";
 
 function fullvalidation(){
     validateEmail();
     validatePassword();
     checkIfPasswordMatch();
     const checkEmail = document.getElementById("errorCheckEmail").innerHTML;
-    const checkPassword= document.getElementById("errorCheckEmail").innerHTML;
-    const checkMatch = document.getElementById("errorCheckEmail").innerHTML;
-    if(!!checkEmail && !!checkPassword && !!checkMatch){
-
+    const checkPassword= document.getElementById("errorCheckPassword").innerHTML;
+    const checkMatch = document.getElementById("errorCheckConfirmedPassword").innerHTML;
+    userName= document.getElementById("firstName").value;
+    sessionStorage.setItem("UserName",userName);
+if(checkEmail =="" && checkPassword=="" && checkMatch==""){
+    window.location.href = "success.html";
+        
+        
     }
     else{
-        window.location.href = "success.html";
+       
     }
 }
 
@@ -26,6 +31,10 @@ function validateEmail(){
         text = "email is not a valid email address";
         document.getElementById("errorCheckEmail").innerHTML = text;
     }
+    else{
+    text ="";
+    document.getElementById("errorCheckEmail").innerHTML = text;
+    }
 }
 
 function validatePassword(){
@@ -36,14 +45,22 @@ function validatePassword(){
         text = "password is not a valid password";
         document.getElementById("errorCheckPassword").innerHTML = text;
     }
+    else{
+    text ="";
+    document.getElementById("errorCheckPassword").innerHTML = text;
+    }
 }
 
 function checkIfPasswordMatch(){
     const passwordInput = document.getElementById("passwordForm").value;
     const confirmPasswordInput = document.getElementById("confirmPasswordForm").value;
-    var text;
-    if(!passwordInput == confirmPasswordInput){
+    var text
+    if(!passwordInput.match(confirmPasswordInput)){
         text = "password doesn't match";
-        document.getElementById("errorCheckConfirmPassword").innerHTML = text;
+        document.getElementById("errorCheckConfirmedPassword").innerHTML = text;
+    }
+    else{
+    text ="";
+    document.getElementById("errorCheckConfirmedPassword").innerHTML = text;
     }
 }
