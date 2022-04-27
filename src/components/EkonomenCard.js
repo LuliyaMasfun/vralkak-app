b89d6ffbe37943bfe7a1428d40795246376bb86a
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-04-27 05:43:49 +0200

    I have moved whole landingpage to React :))

---
+++ src/components/EkonomenCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Body.css';
+import './FoodCard.css';
+import ekonomenShadow from './assets/shadowToEkonomen.png';
+import ekonomenFood from './assets/ekonomenFood.svg';
+import imgDescription1 from './assets/pictureToDescriptionV1.png';
+import imgDescription2 from './assets/pictureToDescriptionV2.png';
+import imgDescription3 from './assets/pictureToDescriptionV3.png';
+import imgDescription4 from './assets/ekonomenDescription4.png';
+import arrow from './assets/Vector.png';
+
+export default function EkonomenCard() {
+  return (
+
+    <div className="ekonomen" id="ekonomen">
+    <div className="foodCardName">“Ekonomen”</div>
+    <img className="ekonomenShadow" src={ekonomenShadow}/>
+    <img className="ekonomenFood"src={ekonomenFood} />
+    <div className="priceBorder">fr. 349 kr</div>
+    <div className="textBelowPicture">Kassen för dig som är ute efter schysst mat till ett schysst pris.</div>
+    <div className="ekonomLine"></div>
+    <div className="foodDescription1"><img className="pictureToDescription1"src={imgDescription1} /><span className="textToDescription1">Mat för två veckor på två timmar</span></div>
+    <div className="foodDescription2"><img className="pictureToDescription2"src={imgDescription2} /><span className="textToDescription2">Arnes chili con carne, Farmors lasagne, Bengans korvstroganoff</span></div>
+    <div className="foodDescription3"><img className="pictureToDescription3"src={imgDescription3} /><span className="textToDescription3">8 min tillagning per portion</span></div>
+
+    <div className="foodDescription4"><img className="pictureToDescriptionEkonomen" src={imgDescription4} /><span className="textToDescriptionEkonomen">Prispressade varor</span></div>
+
+    <button className="ekonomenCallToAction" onclick="location.href='/LoginPage/LoginPage.html'">Beställ</button>
+    <img className="orderArrowRight1" src={arrow} />   
+    </div>
+
+  )
+}
---
+++ src/components/VeganenCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Body.css';
+import './FoodCard.css';
+import veganenShadow from './assets/shadowToEkonomen.png';
+import veganenFood from './assets/veganFood.png';
+import imgDescription1 from './assets/pictureToDescriptionV1.png';
+import imgDescription2 from './assets/pictureToDescriptionV2.png';
+import imgDescription3 from './assets/pictureToDescriptionV3.png';
+import imgDescription4 from './assets/pictureToDescriptionV4.png';
+import arrow from './assets/Vector.png';
+
+export default function VeganenCard() {
+  return (
+    <div class="foodCard" id="veganen">
+        <div class="foodCardName">“Veganen”</div>
+        <img src={veganenShadow} class="ekonomenShadow"/>
+        <img class="ekonomenFood" id="veganFood" src={veganenFood} />
+        <div class="priceBorder">fr. 389 kr</div>
+        <div class="textBelowPicture">Kassen för dig som oavsett anledning valt en en växtbaserad kost.</div>
+        <div class = "ekonomLine"></div> 
+        <div class="foodDescription1"><img class="pictureToDescription1"src={imgDescription1} /><span class="textToDescription1">Mat för 2 veckor på 2 timmar</span></div>
+        <div class="foodDescription2"><img class="pictureToDescription2"src={imgDescription2} /><span class="textToDescription2">Het vegansk chili, Zaras zucchinilasagne, Sonjas sojastroganoff</span></div> 
+        <div class="foodDescription3"><img class="pictureToDescription3"src={imgDescription3} /><span class="textToDescription3">8 min tillagning per portion</span></div>
+        <div class="foodDescription4"><img class="pictureToDescriptionVeganen"src={imgDescription4} /><span class="textToDescriptionVeganen">Helt fritt från animaliska produkter</span></div>
+
+        <button class="veganenCallToAction" onclick="location.href='/LoginPage/LoginPage.html'">Beställ</button>
+        <img class="orderArrowRight3" src={arrow} />  
+    </div>
+  )
+}
