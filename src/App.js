342721657e3bb6f4bda7841a7ba5ed6641a2a65f
Ograeset <tedros963@gmail.com>
2022-04-28 17:08:19 +0200

    uncompleted routing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import EkonomenCard from './components/EkonomenCard';
 import GretaCard from './components/GretaCard';
 import VeganenCard from './components/VeganenCard';
 import GuidePage from './components/GuidePage';
+import LogInPage from './components/LogInPage';
+import SignUpPage from './components/SignUpPage';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Navbar />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/LoginPage" component={LogInPage} />
+        <Route path="/SignUpPage" component={SignUpPage} />
+      </Switch>
+    </BrowserRouter>
+    
+    
+    
+    
+    
+    {/* 
     <HomePage/>
     <EkonomenCard/>
     <GretaCard/>
-    <VeganenCard/>
+    <VeganenCard/> */}
     
     </>
     
